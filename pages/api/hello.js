7d79920669a286555f79a2a47e966192b1219c40
hitfishking <1063888400@qq.com>
2022-06-04 15:58:32 +0800

    ingest data

---
+++ pages/api/hello.js
@@ -0,0 +1,7 @@
+import { connectToElasticsearch } from '../../lib/elasticsearch'
+
+export default async function hello (req, res) {
+  const client = await connectToElasticsearch()
+  const { body } = await client.info()
+  res.status(200).json({ body })
+}
